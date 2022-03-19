33207e2b6c077ce1680c12f08aacfb67ea64e747
DamjanSudarevic <damjan.sudarevic@protonmail.com>
2022-03-19 19:15:31 +0100

    Hosted at damjansudarevic.github.io/Typescript-2048/

--- app.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const app = express();
-app.use(express.static("public"));
-app.get('/', (req, res) => {
-    res.send('Well done!');
-});
-app.listen(8080, () => {
-    console.log('The application is listening on port 8080!');
-});
